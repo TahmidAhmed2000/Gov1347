e7f92faf30f6808388619891d6c035014784772b
Tahmid Ahmed <tahmidahmed@college.harvard.edu>
2020-09-13 20:31:56 -0400

    created various plots

--- scripts/week_1.R
+++ scripts/week_1.R
@@ -1,14 +1,26 @@
 ## install via `install.packages("name")`
+## Blog_1 code
 library(tidyverse)
-library(ggplot2)
 library(usmap)
+library(ggplot2)
+library(ggrepel)
+library(skimr)
+library(gt)
 
+#created my own theme
+theme_tahmid <- theme_classic() + 
+  theme(panel.border = element_blank(),
+        plot.title   = element_text(size = 12, hjust = 0.5), 
+        axis.text.x  = element_text(angle = 45, hjust = 1),
+        axis.text    = element_text(size = 10),
+        strip.text   = element_text(size = 15),
+        axis.line    = element_line(colour = "blue"),
+        legend.position = "top",
+        legend.text = element_text(size = 11),
+        plot.subtitle = element_text(size = 12, hjust = 0.5))
+
+# Read popular voting data
 popvote_df <- read_csv("data/popvote_1948-2016.csv")
-
-## subset
-popvote_df %>% 
-  filter(year == 2016) %>% 
-  select(party, candidate, pv2p)
 
 ## format
 (popvote_wide_df <- popvote_df %>%
@@ -20,56 +32,12 @@ popvote_df %>%
     mutate(winner = case_when(democrat > republican ~ "D",
                               TRUE ~ "R")))
 
-## summarise
-popvote_wide_df %>% 
-  group_by(winner) %>%
-  summarise(races = n())
-
 ####----------------------------------------------------------#
 #### Visualize trends in national pres pop vote ####
 ####----------------------------------------------------------#
 
-## example: histogram
-ggplot(popvote_df, aes(x = pv2p)) + 
-  geom_histogram()
-
-## example: barplot (+ custom colors)
-ggplot(popvote_df, aes(x = year, y = pv2p, fill = party)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = c("blue", "red")) 
-
-## example: lineplot (+ custom colors + nicer theme)
-ggplot(popvote_df, aes(x = year, y = pv2p, colour = party)) +
-  geom_line() +
-  scale_color_manual(values = c("blue", "red")) + 
-  theme_bw()
-
-## BAD plot: 
-## dark background, "too much ink", no legend, small font, 
-ggplot(popvote_df, aes(x = year, y = pv2p, colour = party)) +
-  geom_line(stat = "identity") + 
-  theme_dark() +
-  theme(legend.position = "none", axis.title = element_text(size = 5))
-
-## GOOD plot:
-## high contrast, "minimal ink", legend, detailed x-ticks, larger font
-ggplot(popvote_df, aes(x = year, y = pv2p, colour = party)) +
-  geom_line(stat = "identity") + 
-  scale_x_continuous(breaks = seq(1948, 2016, 4)) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45))
 
 ## EXCELLENT plot:
-## "pretty" customized theme
-my_pretty_theme <- theme_bw() + 
-  theme(panel.border = element_blank(),
-        plot.title   = element_text(size = 15, hjust = 0.5), 
-        axis.text.x  = element_text(angle = 45, hjust = 1),
-        axis.text    = element_text(size = 12),
-        strip.text   = element_text(size = 18),
-        axis.line    = element_line(colour = "black"),
-        legend.position = "top",
-        legend.text = element_text(size = 12))
 
 ggplot(popvote_df, aes(x = year, y = pv2p, colour = party)) +
   geom_line(stat = "identity") +
@@ -78,7 +46,7 @@ ggplot(popvote_df, aes(x = year, y = pv2p, colour = party)) +
   ylab("popular vote %") +
   ggtitle("Presidential Vote Share (1948-2016)") + 
   scale_x_continuous(breaks = seq(from = 1948, to = 2016, by = 4)) +
-  my_pretty_theme
+  theme_tahmid
 
 ## saves last displayed plot
 ggsave("figures/PV_national_historical.png", height = 4, width = 8)
@@ -106,6 +74,7 @@ pv_win_map <- pvstate_df %>%
   filter(year == 2000) %>%
   mutate(winner = ifelse(R > D, "republican", "democrat"))
 
+## map: wins bty state
 plot_usmap(data = pv_win_map, regions = "states", values = "winner") +
   scale_fill_manual(values = c("blue", "red"), name = "state PV winner") +
   theme_void()
@@ -115,6 +84,7 @@ pv_margins_map <- pvstate_df %>%
   filter(year == 2000) %>%
   mutate(win_margin = (R_pv2p-D_pv2p))
 
+## map win margins by state
 plot_usmap(data = pv_margins_map, regions = "states", values = "win_margin") +
   scale_fill_gradient2(
     high = "red", 
@@ -132,6 +102,7 @@ pv_map_grid <- pvstate_df %>%
   filter(year >= 1980) %>%
   mutate(winner = ifelse(R > D, "republican", "democrat"))
 
+## map grid by states
 plot_usmap(data = pv_map_grid, regions = "states", values = "winner", color = "white") +
   facet_wrap(facets = year ~.) + ## specify a grid by year
   scale_fill_manual(values = c("blue", "red"), name = "PV winner") +
@@ -139,45 +110,86 @@ plot_usmap(data = pv_map_grid, regions = "states", values = "winner", color = "w
   theme(strip.text = element_text(size = 12),
         aspect.ratio=1)
 
+## save plot of historical trends
 ggsave("figures/PV_states_historical.png", height = 3, width = 8)
 
-####----------------------------------------------------------#
-#### Extra: FiveThirtyEight replication in ggplot2 ####
-#### https://projects.fivethirtyeight.com/swing-states-2020-election/
-####----------------------------------------------------------#
 
-pvstate_df$vote_margin <- pvstate_df$R_pv2p - pvstate_df$D_pv2p
+## Read Electoral College Data of 2016
+evstate2016_df <- read_csv("data/electoralvote2016.csv") %>%
+  mutate(id = 1:51)
 
-pvstate_df %>% 
-  ## subset data
-  filter(state %in% c("Arizona","Georgia","Texas")) %>%
-  filter(year >= 2000) %>%
-  ## pipe into ggplot()
-  ggplot(aes(x=year, y=vote_margin, color=vote_margin)) + 
-  ## specify a grid by state
-  facet_wrap(. ~ state) + 
-  ## add plot elements
-  geom_hline(yintercept=0,color="gray") +
-  geom_line(size=2) + 
-  geom_point(size=6) +
-  ## specify scale colors
-  scale_colour_gradient(low = "blue", high = "red") +
-  scale_fill_gradient(low = "blue", high = "red") +
-  ## specify titles, labels
-  xlab("") +
-  ylab("Republican vote-share margin") + 
-  ggtitle("Swing states that moved sharply to the left in 2016") +
-  ## switch position of x-axis and y-axis
-  coord_flip() +
-  ## make x-axis (year) run from top to bottom
-  scale_x_reverse(breaks=unique(pvstate_df$year)) +
-  theme_minimal() + 
-  theme(panel.border    = element_blank(),
-        plot.title      = element_text(size = 20, hjust = 0.5, face="bold"), 
-        legend.position = "none",
-        axis.title      = element_text(size=18),
-        axis.text.x     = element_text(angle = 45, hjust = 1),
-        axis.text       = element_text(size = 18),
-        strip.text      = element_text(size = 18, face = "bold"))
+# filter for 2016 presidential election for popular vote
+pv_margins_map_2016 <- pvstate_df %>%
+  filter(year == 2016) %>%
+  mutate(win_margin = (D_pv2p-R_pv2p)) %>%
+  mutate(id = 1:51) %>%
+  mutate(democrat_margin_pop = D - R)
 
+# join both electoral college data and popular vote data
+joineddata <- inner_join(pv_margins_map_2016, evstate2016_df, by = "id")
+
+# Plot 2016 win margin map, using a Red to Blue Scale and Purple as middle to
+# represent current themes
+plot_usmap(data = pv_margins_map_2016, regions = "states", values = "win_margin") +
+  scale_fill_gradient2(
+    high = "blue", 
+    mid = scales::muted("purple"),
+    low = "red", 
+    breaks = c(-50,-25,0,25,50), 
+    limits = c(-52,50),
+    name = "Win margin"
+  ) +
+  theme_void() + 
+  labs(title = "Popular Voting Share Margins in 2016 Election",
+       subtitle = "Popular Voting Shares in a 2-Party System",
+       caption = "Data taken from Gov 1347 Lab") + 
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        plot.subtitle = element_text(size = 12, hjust = 0.5))
+
+## Save popular voting margins amp
+ggsave("figures/PV_states_2016.png", height = 4, width = 8)
+
+
+## Plot electoral college states with simialr themes from popular vote
+plot_usmap(data = evstate2016_df, regions = "states", values = "win_margin") +
+  scale_fill_gradient2(
+    high = "blue", 
+    mid = scales::muted("purple"),
+    low = "red", 
+    breaks = c(-50,-25,0,25,50), 
+    limits = c(-52,55),
+    name = "Win margin"
+  ) +
+  theme_void() + 
+  labs(title = "2016 Presidential Electoral Vote Share Win Margin",
+       subtitle = "2-party Electoral College Vote share",
+       caption = "Data collected from Federal Election Commission.") +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        plot.subtitle = element_text(size = 12, hjust = 0.5))
+
+## Save popular voting margins amp
+ggsave("figures/EV_states_2016.png", height = 4, width = 8)
+
+## Plot democratic margins in both opular vote and electoral college against
+## each other
+joineddata %>%
+  ggplot(aes(democrat_margin_pop, win_margin.y)) + 
+  geom_point() + 
+  geom_smooth(method='lm') +
+  labs(title = "2016 Democratic Margins in Popular Vote and Electoral Vote",
+       subtitle = "Regression Plot of Voting Margins",
+       caption = "Data collected from joined data set",
+       y = "Electoral Vote Margin",
+       x = "Popular Vote Margin") +
+  theme_tahmid
+
+## save resulting regression plot
+ggsave("figures/regmargins_states_2016.png", height = 4, width = 8)
+
+
+## Ran regression of both types of margins
+democrat_reg <- lm(win_margin.y ~ democrat_margin_pop, data = joineddata)
+
+## Evaluate summary of regression
+demregsummary <- summary(democrat_reg)
 
