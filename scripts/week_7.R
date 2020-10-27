d68aff0e8702fc1578beeae06aae4f0568b587cd
Tahmid Ahmed <tahmidahmed@college.harvard.edu>
2020-10-27 05:27:35 -0400

    edited code

--- scripts/week_7.R
+++ scripts/week_7.R
@@ -9,33 +9,16 @@ library(sjPlot)
 
 # Read in data
 covid <- read_csv("data/covid_state.csv") %>%
-  filter(grepl('2020-10-25', date))
+  filter(grepl('2020-10-25', date)) %>%
+  mutate(pos_pct = positive/totalTestResults)
 
 poll <- read_csv("data/statepoll_2020.csv") 
 
+# Join Covid and Polling data
 covid_poll <- covid %>%
-  left_join(poll, by = "state") 
+  inner_join(poll, by = "state") 
 
-ggplot(covid_poll, aes(x = positive, y = Trump_pct, label = state)) + 
-  geom_point() +
-  theme_classic() +
-  geom_smooth(method = "lm", formula = y ~ x) +
-  labs(title = "The Effect of Number of Covid Cases on Polling Average",
-       x = "Positive Cases",
-       y = "Popular Vote for Incumbent",
-       color = "",
-       caption = "Source: 270toWin, The Covid Tracking Project") 
-
-ggplot(covid_poll, aes(x = death, y = Trump_pct, label = state)) + 
-  geom_point() +
-  theme_classic() +
-  geom_smooth(method = "lm", formula = y ~ x) +
-  labs(title = "The Effect of Number of Covid Deaths on Polling Average",
-       x = "Deaths",
-       y = "Popular Vote for Incumbent",
-       color = "",
-       caption = "Source: 270toWin, The Covid Tracking Project") 
-
+# Graphs to show Covid Variables effects on Trump's Incumbency Share
 facet_covid = c("death", 
                 "deathIncrease", 
                 "hospitalizedIncrease",
@@ -61,26 +44,28 @@ Facet_corona %>%
   geom_smooth(method = "lm", formula = y ~ x) +
   ylab("Incumbent Popular Vote Share") +
   xlab("Covid Related Variables") +
+  labs(caption = "Source: 270toWin, The Covid Tracking Project" ) +
   facet_wrap(~key, ncol = 5, scales = "free") +
   theme_classic()+
   theme(legend.position = "none")
 
 ggsave("Incumbent_covid.png", height = 10, width = 12)
 
+# Linear Covid Model to analyze Covid effects
 covid_model <- lm(Trump_pct ~ deathIncrease + positiveIncrease + totalTestResultsIncrease + negativeIncrease +
                               hospitalizedIncrease, data = covid_poll)
 summary((covid_model))
 
-stargazer(covid_model,
-          title = "Covid Effects on Trump's Vote Share",
-          header = FALSE,
-          covariate.labels = c("deathIncrease", "positiveIncrease", "negativeIncrease", "totalTestResultsIncrease", "hospitalizedIncrease"),
-          dep.var.labels = "Incumbent Vote Share",
-          omit.stat = c("f", "rsq"),
-          notes.align = "l",
-          font.size = "tiny",
-          column.sep.width = "1pt")
-
 tab_model(covid_model,
           title = "The Impact of COVID Cases on Trump's Voteshare by State",
           dv.labels = "Trump's Projected State Voteshare")
+
+# Graph to show Covid positivity rates by state
+ggplot(covid_poll, aes(state = state, fill = pos_pct)) + 
+  geom_statebins() + 
+  theme_statebins() +
+  labs(title = "Covid Positivity Rate by State",
+       caption = "The Covid Tracking Project") 
+
+ggsave("state_covid.png", height = 8, width = 12)
+
